8776d426373c1fc928704f7f9d1c5a75b35587b2
TechnoStudy <ismettechno@gmail.com>
2024-09-23 19:46:52 +0100

    Login BeforeClass a eklendi

---
+++ src/Gun01/_08_SubscribeNewsLetter.java
@@ -0,0 +1,19 @@
+package Gun01;
+
+/*
+  Senaryo
+  1- Siteyi açınız.
+  2- Newsletter  Subscribe işlemini abone lunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
+  3-
+  4-
+ */
+
+import Utlity.BaseDriver;
+
+public class _08_SubscribeNewsLetter extends BaseDriver {
+
+
+
+
+
+}
