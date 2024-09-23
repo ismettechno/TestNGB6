1cc677815cbdf1aa62c8de8e96f85e8f1732c2af
TechnoStudy <ismettechno@gmail.com>
2024-09-23 18:38:26 +0100

    BaseDriver ve Tools hazırlandı

---
+++ src/Gun01/_05_BaseDriverTest.java
@@ -0,0 +1,14 @@
+package Gun01;
+
+import Utlity.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _05_BaseDriverTest extends BaseDriver {
+
+    @Test
+    public void Test1()
+    {
+        driver.get("https://www.google.com/");
+    }
+
+}
