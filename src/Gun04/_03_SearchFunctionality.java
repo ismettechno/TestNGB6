305a274c582fec3565c62d2489c16f6bf461f835
TechnoStudy <ismettechno@gmail.com>
2024-09-26 18:49:21 +0100

    _03_SearchFunctionality

--- src/Gun04/_03_SearchFunctionality.java
+++ src/Gun04/_03_SearchFunctionality.java
@@ -9,5 +9,43 @@ package Gun04;
     */
 
 
-public class _03_SearchFunctionality {
+import Utlity.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _03_SearchFunctionality extends BaseDriver {
+
+    @Test
+    @Parameters("searchText")
+    public void SearchFunction(String arananKelime)
+    {
+        WebElement searchInput=driver.findElement(By.name("search"));
+        searchInput.sendKeys(arananKelime+ Keys.ENTER);
+
+        List<WebElement> captions=driver.findElements(By.cssSelector("[class='caption']>h4"));
+
+        for(WebElement e : captions)
+            Assert.assertTrue(e.getText().toLowerCase().contains(arananKelime.toLowerCase()),
+                    "Aranan kelime bulunamadı");
+
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
