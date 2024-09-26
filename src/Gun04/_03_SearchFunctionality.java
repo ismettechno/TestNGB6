091addd75f3c9388c1c0791981efac3178b0e34e
TechnoStudy <ismettechno@gmail.com>
2024-09-26 18:03:11 +0100

    _01_ContactUs

---
+++ src/Gun04/_03_SearchFunctionality.java
@@ -0,0 +1,13 @@
+package Gun04;
+
+/*
+      Senaryo ;
+      1- Siteyi açınız.
+      2- mac kelimeini göndererek aratınız.
+      3- Çıkan sonuçlarda mac kelimesinin geçtiğini doğrulayınız.
+      4- aynı işlemi samsung için de yapınız
+    */
+
+
+public class _03_SearchFunctionality {
+}
