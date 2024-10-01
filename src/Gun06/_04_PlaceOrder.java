2943c59e07e22a874fcc3ba0b2d0b7754d4dda6c
TechnoStudy <ismettechno@gmail.com>
2024-10-01 18:50:05 +0100

    _04_PlaceOrder

---
+++ src/Gun06/_04_PlaceOrder.java
@@ -0,0 +1,16 @@
+package Gun06;
+
+/*
+   Senaryo :
+   1- Siteyi açınız.
+   2- Sitede "ipod" kelimesini aratınız
+   3- Çıkan sonuçlardan ilkini sepete atınız.
+   4- Shopping Chart a tıklatınız.
+   5- Checkout yapınız.
+   6- Continue butonalarına tıklatıp bilgileri giriniz.
+   7- Confirm ile siparişi verdiğinizi doğrulayınız.
+      doğrulamayı çıkan sayfadaki "Your order has been placed" yazısı ile yapınız.
+*/
+
+public class _04_PlaceOrder {
+}
