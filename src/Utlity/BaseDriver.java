112f58c321d111428483e8b5f10553712ebc20d4
TechnoStudy <ismettechno@gmail.com>
2024-10-03 18:17:26 +0100

    log4j Tanımlandı

--- src/Utlity/BaseDriver.java
+++ src/Utlity/BaseDriver.java
@@ -1,5 +1,8 @@
 package Utlity;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -14,6 +17,7 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class BaseDriver {
+    public static Logger LogEkle = LogManager.getLogger(); //Logları ekleyeceğim kuyruğu başlattım.
     public static WebDriver driver;
     public static WebDriverWait wait;
 
